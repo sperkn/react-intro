d3f8dcbf4948b436288fe41093a0c768a8b51da2
sperkn <nicholas.kreps@gmail.com>
2018-11-10 17:50:24 -0500

    added header, inventory and order components & added markup to header with props for tagline

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 class StorePicker extends React.Component {
-    // constructor(props){
-
-    // }
-
-    render() {
-        return <p>Hello!</p>
-    }
+  render() {
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store -></button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
