8ec21c842055edf1778c4589e094297ac171c7a5
sperkn <nicholas.kreps@gmail.com>
2018-11-10 19:03:09 -0500

    updated StorePicker component with onSubmit action and ref property for retrieving input from user

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this); 
+  // }
+
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from that input
+    console.log(this.myInput);
+    // 3. Change the page to /store/whatever-they-entered
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" ref={this.myInput} required placeholder="Store Name" />
         <button type="submit">Visit Store -></button>
       </form>
     );
