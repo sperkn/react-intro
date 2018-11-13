dc38c6b4bc9b38bd696a3687bcc5e3343f47d126
sperkn <nicholas.kreps@gmail.com>
2018-11-13 12:45:31 -0500

    updated App and added Fish Components to use state to load up menu with sample fishes

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -7,9 +7,10 @@ class Inventory extends React.Component {
       <div className="inventory">
         <h2>Inventory</h2>
         <AddFishForm addFish={this.props.addFish} />
+        <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
       </div>
     );
-  }
+  } 
 }
 
 export default Inventory;
